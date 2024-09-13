55b1f2aeeac74e2c8af035b0894bba9d0da3c77b
Kristen.Phan <kristen.phan@rabobank.nl>
2024-09-13 14:46:13 +0200

    spring init

---
+++ src/main/java/com/example/idempotentfooddispenser/IdempotentFoodDispenserApplication.java
@@ -0,0 +1,13 @@
+package com.example.idempotentfooddispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentFoodDispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentFoodDispenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idempotentfooddispenser/IdempotentFoodDispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idempotentfooddispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentFoodDispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
